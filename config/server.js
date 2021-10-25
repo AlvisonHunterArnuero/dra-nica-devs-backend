db6e4c99bd0fc5140045c1385e597d83b98cea56
Alvison Hunter <alvison.hunter@reeftechnology.com>
2021-10-25 13:46:14 -0600

    Initial Commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '7016498e97d8d3e9251982ba66b93bba'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'd3498476-155a-48b2-9f8b-bc5495d9aa75'
+};
